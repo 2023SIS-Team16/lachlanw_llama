d144bd99b52b502d0f83c66d098c402a8c6a1a48
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2023-09-03 14:00:17 +1000

    Improved formatting of response

---
+++ README.md
@@ -0,0 +1,1 @@
+Use a model from: https://huggingface.co/TheBloke/CodeLlama-13B-GGUF/tree/main
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from llama_cpp import Llama
-
 model = Llama(model_path="./model/codellama-13b.Q2_K.gguf") #Replace with whatever model you are using
+def get_response(to_fix):
+    print("TODO!")
+    response = model(f"Q: What is the correct spelling of '{to_fix}' A:", stop=["Q:", "\n"], max_tokens=8, echo=True)
+    to_remove = f"Q: What is the correct spelling of '{to_fix}' A:"
+    print(response)
+    result = response['choices'][0]['text']
+
+    print(result)
+    end_index = len(to_fix) + 40
+    result = result[end_index:].strip().strip("'")
+
+    return result
 
 to_fix = "What ae you dxing"
 
-output = model("Q: Who is the prime minister of Australia? A:", max_tokens=8, echo=True)
-#output = model(f"Break the following into several grammatically correct words, correcting spelling errors: '{to_fix}' ", max_tokens=32, stop=["Break", "\n"], echo=True)
-print(output)
-print("TEST")
-#Error caused by using GGML3 version instead of GGUF
+print(get_response("What ae you d ing"))
 
-def get_response(to_fix):
-    print("TODO!")
-    response = model(f"Break the following into several grammatically correct words, correcting spelling errors: '{to_fix}' ", max_tokens=32, stop=["Q:", "\n"], echo=True)
-    return response
