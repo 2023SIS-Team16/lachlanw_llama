496c993af9f4f419529f98ba0701d61d2ee76d3d
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2023-09-06 04:23:24 +1000

    Functional WS implementation

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ model = Llama(model_path="./model/codellama-13b.Q2_K.gguf") #Replace with whatev
 
 async def req_handler(websocket, path):
     data = await websocket.recv()
-
+    data = data[7:]
+    print(data)
+    res = conversion.get_response(data, model)
+    print(res)
+    await websocket.send(res)
+    
 #Running the server
 print("Server starting.")
-server = websockets.serve(req_handler, "localhost", 7000)
+server = websockets.serve(req_handler, "localhost", 8000)
 asyncio.get_event_loop().run_until_complete(server)
 asyncio.get_event_loop().run_forever()
 print("This shouldn't run.")
