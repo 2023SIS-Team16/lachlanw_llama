bd30baeedfde6ff7d5b7090940de8399afc9ec10
BrendanKhavin <b.khavin@gmail.com>
2023-09-06 11:26:12 +1000

    Added unit tests to model testing file

--- modeltesting.py
+++ modeltesting.py
@@ -1,9 +1,44 @@
 import conversion
 from llama_cpp import Llama
-model = Llama(model_path="model/codellama-13b.Q2_K.gguf") #Replace with whatever model you are using
+import unittest
 
-print(conversion.get_response("what ae you d ing", model))
-print(conversion.get_response("hmllo there", model))
-print(conversion.get_response("yesi think so", model))
-print(conversion.get_response("my name is lchlan", model))
-print(conversion.get_response("are yousu re about tht", model))
+class TestConversion(unittest.TestCase):
+    
+    def setUp(self):
+        self.model = Llama(model_path="./model/codellama-13b.Q2_K.gguf")
+
+    def test_get_response_normal_case(self):
+        # Arrange
+        input_str = "hmllo there"
+        expected_output = "hello there"
+
+        # Act
+        actual_output = conversion.get_response(input_str, self.model)
+
+        # Assert
+        self.assertEqual(actual_output, expected_output)
+
+    def test_get_response_edge_case(self):
+        # Arrange
+        input_str = "wh t ae you d ing"
+        expected_output = "what are you doing"
+
+        # Act
+        actual_output = conversion.get_response(input_str, self.model)
+
+        # Assert
+        self.assertEqual(actual_output, expected_output)
+
+    def test_get_response_asl_confusion(self):
+        # Arrange
+        input_str = "m and n are oftem comfused in asl"
+        expected_output = "m and n are often confused in asl"
+
+        # Act
+        actual_output = conversion.get_response(input_str, self.model)
+
+        # Assert
+        self.assertEqual(actual_output, expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
