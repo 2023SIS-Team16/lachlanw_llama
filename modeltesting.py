7f4567c6a01a0dcb954cead2f605bb5124e51b88
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2023-09-13 08:12:54 +1000

    Added additional test cases

--- modeltesting.py
+++ modeltesting.py
@@ -40,5 +40,37 @@ class TestConversion(unittest.TestCase):
         # Assert
         self.assertEqual(actual_output, expected_output)
 
+    def test_get_response_confused_letters_2(self):
+        # Arrange
+        input_str = "in a big fam of tranpolimes"
+        expected_output = "im a big fan of trampolines"
+
+        # Act
+        actual_output = conversion.get_response(input_str, self.model)
+
+        # Assert
+        self.assertEqual(actual_output, expected_output)
+
+    def test_get_response_confused_letters_with_typo(self):
+        # Arrange
+        input_str = "i thimk itis rather immocent"
+        expected_output = "i think it is rather innocent"
+
+        # Act
+        actual_output = conversion.get_response(input_str, self.model)
+
+        # Assert
+        self.assertEqual(actual_output, expected_output)
+
+    def test_get_response_no_spacing(self):
+        # Arrange
+        input_str = "theweatherisnicetoday"
+        expected_output = "the weather is nice today"
+
+        # Act
+        actual_output = conversion.get_response(input_str, self.model)
+
+        # Assert
+        self.assertEqual(actual_output, expected_output)
 if __name__ == '__main__':
     unittest.main()
