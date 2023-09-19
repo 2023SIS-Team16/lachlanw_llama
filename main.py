a734391341b118cd0667ce876b3b6844b9b32cec
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2023-09-20 01:28:30 +1000

    Prelmin REST impl., refactored testing suite

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
-import conversion
-import asyncio
-import websockets
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from llama_cpp import Llama
 model = Llama(model_path="./model/codellama-13b.Q5_K_M.gguf") #Replace with whatever model you are using
 
-async def req_handler(websocket, path):
-    data = await websocket.recv() #Wait for a translation request
-    data = data[7:] #Filter out the request's preamble
-    print(data)
-    res = conversion.get_response(data, model)
-    print(res)
+#New server (child of BaseHTTPRequestHandler)
+class Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+            self.send_response(200)
 
-    #Check if the conversion passed or failed.
-    if res == data:
-        await websocket.send(f"FAIL: {res}")
-    else:
-        await websocket.send(f"PASS: {res}")
-    
-#Running the server
-print("Server starting.")
-server = websockets.serve(req_handler, "localhost", 8000)
-asyncio.get_event_loop().run_until_complete(server)
-asyncio.get_event_loop().run_forever()
-print("This shouldn't run.")
+if __name__ == "__main__":
+    webServer = HTTPServer(("localhost", 8080), Server)
+
--- modeltesting.py
+++ modeltesting.py
@@ -2,75 +2,36 @@ import conversion
 from llama_cpp import Llama
 import unittest
 
+""" def compare_strings(unittest, input, expected):
+    output = conversion.get_response(input, model)
+    unittest.assertEqual(output, expected) """
+
 class TestConversion(unittest.TestCase):
-    
-    def setUp(self):
-        self.model = Llama(model_path="./model/codellama-13b.Q5_K_M.gguf")
+    @classmethod
+    def setUpClass(cls):
+        cls.model = Llama(model_path="./model/codellama-13b.Q5_K_M.gguf")
+
+    def compare_strings(self, input, expected):
+        output = conversion.get_response(input, self.model)
+        self.assertEqual(output, expected)
 
     def test_get_response_normal_case(self):
-        # Arrange
-        input_str = "hmllo there"
-        expected_output = "hello there"
-
-        # Act
-        actual_output = conversion.get_response(input_str, self.model)
-
-        # Assert
-        self.assertEqual(actual_output, expected_output)
-
+        self.compare_strings("hmllo there", "hello there")
+        
     def test_get_response_multiple_typos_case(self):
-        # Arrange
-        input_str = "wh t ae you d ing"
-        expected_output = "what are you doing"
-
-        # Act
-        actual_output = conversion.get_response(input_str, self.model)
-
-        # Assert
-        self.assertEqual(actual_output, expected_output)
+        self.compare_strings("wh t ae you d ing", "what are you doing")
 
     def test_get_response_confused_letters(self):
-        # Arrange
-        input_str = "m and n are oftem comfused in asl"
-        expected_output = "m and n are often confused in asl"
-
-        # Act
-        actual_output = conversion.get_response(input_str, self.model)
-
-        # Assert
-        self.assertEqual(actual_output, expected_output)
+        self.compare_strings("m and n are oftem comfused in asl", "m and n are often confused in asl")
 
     def test_get_response_confused_letters_2(self):
-        # Arrange
-        input_str = "in a big fam of tranpolimes"
-        expected_output = "im a big fan of trampolines"
-
-        # Act
-        actual_output = conversion.get_response(input_str, self.model)
-
-        # Assert
-        self.assertEqual(actual_output, expected_output)
+        self.compare_strings("in a big fam of tranpolimes", "im a big fan of trampolines")
 
     def test_get_response_confused_letters_with_typo(self):
-        # Arrange
-        input_str = "i thimk itis rather immocent"
-        expected_output = "i think it is rather innocent"
-
-        # Act
-        actual_output = conversion.get_response(input_str, self.model)
-
-        # Assert
-        self.assertEqual(actual_output, expected_output)
+        self.compare_strings("i thimk itis rather immocent", "i think it is rather innocent")
 
     def test_get_response_no_spacing(self):
-        # Arrange
-        input_str = "theweatherisnicetoday"
-        expected_output = "the weather is nice today"
+        self.compare_strings("theweatherisnicetoday", "the weather is nice today")
 
-        # Act
-        actual_output = conversion.get_response(input_str, self.model)
-
-        # Assert
-        self.assertEqual(actual_output, expected_output)
 if __name__ == '__main__':
     unittest.main()
