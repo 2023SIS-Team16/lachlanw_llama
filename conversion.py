7c7aa393bcad8ae0d2327ff33cf83f1f7160fb2b
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2023-09-20 01:46:00 +1000

    Added whitespace stripping, more test cases

--- conversion.py
+++ conversion.py
@@ -1,6 +1,6 @@
 from difflib import SequenceMatcher
 
-def get_response(to_fix, model, keep_punctuation=False):
+def get_response(to_fix, model, keep_punctuation=False, enable_logging=False):
     response = model(f"Q: Proofread the following sentence, without changing its structure, or making significant changes to words: '{to_fix}'. A: The correct form of this sentence is:", stop=["is:", "\n"], max_tokens=32, echo=True)
     #(+40) Q: What is the correct spelling of '{to_fix}' A:
     #(+64) Q: Update the following sentence to fix any typos: '{to_fix}' A:
@@ -11,26 +11,32 @@ def get_response(to_fix, model, keep_punctuation=False):
     end_index = len(to_fix) + 153 #NOTE: To update this if the prompt is changed, subtract 8 ({to_fix}) from the new string's length
     result = result[end_index:].strip()
     similarity_score = get_similarity(to_fix, result)
-    print(to_fix)
-    print(result)
-    print(similarity_score)
+
+    if enable_logging:
+        print(to_fix)
+        print(result)
+        print(similarity_score)
 
     if(keep_punctuation == False):
         result = result.lower()
         result = ''.join(filter(filter_punctuation, result))
+
+    result = result.strip() #Remove whitespace (' example ' => 'example')
 
     if len(to_fix) * 1.5 < len(result) or len(to_fix) * 0.8 > len(result) or similarity_score < 0.65:
         return to_fix #Just return original string if the response dramatically alters the sentence's length
     else:
         return result #Return fixed string
     
-def get_similarity(original, fixed):
+def get_similarity(original, fixed, enable_logging=False):
     #Converts both strings to lowercase, and removes puncuation to make the comparison 'fairer'
-    print("Initial: " + fixed)
+    if enable_logging:
+        print("Initial: " + fixed)
     original = original.lower()
     fixed = fixed.lower()
     fixed = ''.join(filter(filter_punctuation, fixed))
-    print("Final: " + fixed)
+    if enable_logging:
+        print("Final: " + fixed)
     return SequenceMatcher(None, original, fixed.lower()).ratio()
 
 def filter_punctuation(char):
--- modeltesting.py
+++ modeltesting.py
@@ -18,6 +18,12 @@ class TestConversion(unittest.TestCase):
     def test_get_response_normal_case(self):
         self.compare_strings("hmllo there", "hello there")
         
+    def test_get_response_normal_case_2(self):
+        self.compare_strings("how afe you", "how are you")
+
+    def test_get_response_normal_case_3(self):
+        self.compare_strings("the wether is nice today", "the weather is nice today")
+
     def test_get_response_multiple_typos_case(self):
         self.compare_strings("wh t ae you d ing", "what are you doing")
 
@@ -28,10 +34,16 @@ class TestConversion(unittest.TestCase):
         self.compare_strings("in a big fam of tranpolimes", "im a big fan of trampolines")
 
     def test_get_response_confused_letters_with_typo(self):
-        self.compare_strings("i thimk itis rather immocent", "i think it is rather innocent")
+        self.compare_strings("i thimk itis rather innocent", "i think it is rather innocent")
+
+    def test_get_response_confused_letters_with_typo_2(self):
+        self.compare_strings("thpt is absolmtely wrong", "that is absolutely wrong")
 
     def test_get_response_no_spacing(self):
         self.compare_strings("theweatherisnicetoday", "the weather is nice today")
+
+    def test_get_response_no_spacing_2(self):
+        self.compare_strings("thissentencehasnospaces", "this sentence has no spaces")
 
 if __name__ == '__main__':
     unittest.main()
