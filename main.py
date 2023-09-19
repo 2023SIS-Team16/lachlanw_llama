0fb5f5702173bd66e96df9641f5e25f39d6fc15f
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2023-09-20 02:55:09 +1000

    Shifted to Flask

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import conversion
+from flask import Flask, request
 from llama_cpp import Llama
-model = Llama(model_path="./model/codellama-13b.Q5_K_M.gguf") #Replace with whatever model you are using
+#model = Llama(model_path="./model/codellama-13b.Q5_K_M.gguf") #Replace with whatever model you are using
+app = Flask(__name__)
 
-#New server (child of BaseHTTPRequestHandler)
-class Server(BaseHTTPRequestHandler):
-    def do_GET(self):
-            self.send_response(200)
+@app.route("/parse_text")
+def parse_text():
+    data = request.get_json()
+    
+    print(data['text']) #data.text - The full text sent from the client
+    print(data['index']) #data.resolved_index - All characters preceding this index have already been 'fixed' and returned
 
-if __name__ == "__main__":
-    webServer = HTTPServer(("localhost", 8080), Server)
+    #Try and find a 'sentence' to translate
+    #If there's >30 characters between index and text length, try and cut it off with a space
+    #If this reaches >85 (without an enabling space), force a hard cutoff
+
+    response = {}
+    response['text'] = "after processing" #Translated text
+    response['status'] = False #Returns if the translation passed or failed. Optional parameter
+    response['index'] = 42 #The index after this response. Changes from req's index if translation occured
+
+    print(response)
+
+    return response
 
