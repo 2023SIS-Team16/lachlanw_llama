0b0a43484301e9acdca13525124512dd57cd272c
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2023-10-11 10:23:49 +1100

    Added conditional second translation pass

--- conversion.py
+++ conversion.py
@@ -2,9 +2,6 @@ from difflib import SequenceMatcher
 
 def get_response(to_fix, model, keep_punctuation=False, enable_logging=False):
     response = model(f"Q: Proofread the following sentence, without changing its structure, or making significant changes to words: '{to_fix}'. A: The correct form of this sentence is:", stop=["is:", "\n"], max_tokens=32, echo=True)
-    #(+40) Q: What is the correct spelling of '{to_fix}' A:
-    #(+64) Q: Update the following sentence to fix any typos: '{to_fix}' A:
-    #(+114) Q: Proofread the following sentence, without changing its structure, or making significant changes to words: '{to_fix}' A:
 
     result = response['choices'][0]['text']
 
@@ -40,11 +37,23 @@ def get_response(to_fix, model, keep_punctuation=False, enable_logging=False):
         if errors == 1:
             result =" ".join(result_array)
 
+    #Try again if not good enough
+    if len(to_fix) * 1.5 < len(result) or len(to_fix) * 0.8 > len(result) or similarity_score < 0.65:
+        response = model(f"Q: Fix the typos in the following sentence: '{to_fix}'. A: The sentence without typos is:", stop=["is:", "\n"], max_tokens=32, echo=True)
+        end_index = len(to_fix) + 81 #NOTE: To update this if the prompt is changed, subtract 8 ({to_fix}) from the new string's length
+        result = result[end_index:].strip()
+        similarity_score = get_similarity(to_fix, result)
+
+    #Give up and return original if we can't get a proper translation
     if len(to_fix) * 1.5 < len(result) or len(to_fix) * 0.8 > len(result) or similarity_score < 0.65:
         result = to_fix #Just return original string if the response dramatically alters the sentence's length
-    
+
     return result #Return fixed string
-    
+
+def typo_fixes(text):
+    print("TEST")
+    response = model(f"Q: Proofread the following sentence, without changing its structure, or making significant changes to words: '{to_fix}'. A: The correct form of this sentence is:", stop=["is:", "\n"], max_tokens=32, echo=True)
+
 def get_similarity(original, fixed, enable_logging=False):
     #Converts both strings to lowercase, and removes puncuation to make the comparison 'fairer'
     if enable_logging:
